14458d04aa91d108dbd44d7f3678a5fb7667bf6c
Pragnesh Suthar <85042722+pragnesh-aus@users.noreply.github.com>
2021-09-27 15:39:23 +1000

    Created next js e auto project

---
+++ components/Layout.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Head from 'next/head';
+import { AppBar, Container, Toolbar, Typography } from '@material-ui/core';
+
+export default function Layout({ children }) {
+  return (
+    <div>
+      <Head> Next e-Auto</Head>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography>eAuto</Typography>
+        </Toolbar>
+      </AppBar>
+      <Container>{children}</Container>
+      <footer>
+        <Typography>All rights reserved. Next eAuto 2021</Typography>
+      </footer>
+    </div>
+  );
+}
