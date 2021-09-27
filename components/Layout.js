e559b38977bc06b8d04684a384d901cb9a6e8dba
Pragnesh Suthar <85042722+pragnesh-aus@users.noreply.github.com>
2021-09-27 16:44:16 +1000

    Created custom style for project
    all custom style contains in utils folder.

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Head from 'next/head';
 import { AppBar, Container, Toolbar, Typography } from '@material-ui/core';
-
+import useStyles from '../utils/styles';
 export default function Layout({ children }) {
+  const classes = useStyles();
   return (
     <div>
       <Head> Next e-Auto</Head>
-      <AppBar position="static">
+      <AppBar position="static" className={classes.navbar}>
         <Toolbar>
           <Typography>eAuto</Typography>
         </Toolbar>
