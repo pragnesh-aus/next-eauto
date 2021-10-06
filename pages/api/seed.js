342379e666e4c785c84c454d72e698650dddf028
Pragnesh Suthar <85042722+pragnesh-aus@users.noreply.github.com>
2021-10-06 23:33:44 +1100

    Seed Sample Users

--- pages/api/seed.js
+++ pages/api/seed.js
@@ -3,12 +3,15 @@ import nc from 'next-connect';
 import Product from '../../models/Product';
 import db from '../../utils/db';
 import data from '../../utils/data';
+import User from '../../models/User';
 
 const handler = nc();
 
 handler.get(async (req, res) => {
   await db.connect();
   //const products = await Product.find({});
+  await User.deleteMany();
+  await User.insertMany(data.users);
   await Product.deleteMany();
   await Product.insertMany(data.products);
   await db.disconnect();
