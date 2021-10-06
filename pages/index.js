176f68a825b28c9464267d54146115b879bc0b8e
Pragnesh Suthar <85042722+pragnesh-aus@users.noreply.github.com>
2021-10-06 22:24:09 +1100

    Update Remove Cart Items

--- pages/index.js
+++ pages/index.js
@@ -13,8 +13,26 @@ import Layout from '../components/Layout';
 //import data from '../utils/data';
 import db from '../utils/db';
 import Product from '../models/Product';
+import axios from 'axios';
+import { useRouter } from 'next/router';
+import { useContext } from 'react';
+import { Store } from '../utils/Store';
+
 export default function Home(props) {
+  const router = useRouter();
+  const { state, dispatch } = useContext(Store);
   const { products } = props;
+  const addToCartHandler = async (products) => {
+    const existItem = state.cart.cartItems.find((x) => x._id === products._id);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+    const { data } = await axios.get(`/api/products/${products._id}`);
+    if (data.countInStock < quantity) {
+      window.alert('Sorry. Product is out of stock');
+      return;
+    }
+    dispatch({ type: 'CART_ADD_ITEM', payload: { ...products, quantity } });
+    router.push('/cart');
+  };
   return (
     <Layout>
       <div>
@@ -37,7 +55,11 @@ export default function Home(props) {
                 </NextLink>
                 <CardActions>
                   <Typography>${products.price}</Typography>
-                  <Button size="small" color="primary">
+                  <Button
+                    size="small"
+                    color="primary"
+                    onClick={() => addToCartHandler(products)}
+                  >
                     Add to cart
                   </Button>
                 </CardActions>
